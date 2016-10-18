d940a6b2cc87396bb694e57d170822737400dc29
Kevin Powell <kevin.powell@averydennison.com>
2016-10-18 16:33:27 -0400

    Inital Check-In of Project Source
    GlobeCo's NEW pattented communications protocol.
    CodeName: Jaunty JarJar

---
+++ HelloGitHub/HelloGitHub.cpp
@@ -0,0 +1,13 @@
+// HelloGitHub.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello GitHub!" << std::endl;
+	return 0;
+}
+
